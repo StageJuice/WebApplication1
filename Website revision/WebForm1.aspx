96b145482a71fae8ae15248eda944763f895dc07
StageJuice <36502516+StageJuice@users.noreply.github.com>
2018-02-27 14:32:03 +0100

    c#

--- Website revision/WebForm1.aspx
+++ Website revision/WebForm1.aspx
@@ -4,12 +4,16 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <script src="Script.js" type="text/javascript"></script>
     <link href="Style.css" rel="stylesheet" type="text/css" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>   
+            <asp:Button ID="btnSearch" runat="server" Text="Zoeken" OnClick="btnSearch_Click" />
+            <asp:Label ID="lblRepo" runat="server" Text="Label"></asp:Label>
+        </div>
         <div class="container">
             <div class="repo"><div class="gh-repositories" data-username="StageJuice"></div></div>
         </div>
--- Website revision/WebForm1.aspx.cs
+++ Website revision/WebForm1.aspx.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+using System.IO;
 
 namespace Website_revision
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/StageJuice/WebApplication1");
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "POST";
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var responseText = streamReader.ReadToEnd();
+
+                lblRepo.Text += responseText;
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
